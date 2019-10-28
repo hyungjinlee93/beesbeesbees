89dc8d91ba54966d11dc0ea8e2c86530a3d8ecb4
Hyungjin Lee <leehyungjin4u@gmail.com>
2019-10-28 13:53:09 -0700

    Omit repeated code

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -2,7 +2,6 @@ var ForagerBee = function() {
   Bee.call(this);
   this.age = 10;
   this.job = 'find pollen';
-  this.color = 'yellow';
   this.canFly = true;
   this.treasureChest = [];
 };
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -2,7 +2,6 @@ var HoneyMakerBee = function () {
   Bee.call(this);
   this.age = 10;
   this.job = 'make honey';
-  this.color = 'yellow';
   this.honeyPot = 0;
 };
 
